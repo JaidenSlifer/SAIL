2b984a081d0fd4eb7fb6c222d9f9bd1fee9b7e2d
JaidenSlifer <jaidenslifer0@gmail.com>
2024-03-20 16:35:36 -0400

    add nltk downloader file

--- textprocessor.py
+++ textprocessor.py
@@ -1,3 +1,9 @@
+# from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+from nltk.corpus import stopwords
+
+from nltk.stem import WordNetLemmatizer
+
 class TextProcessor:
 
   def __init__(self):
@@ -7,7 +13,12 @@ class TextProcessor:
     pass
   
   # tokenize, lemmatize text
-  # perhaps do nltk vader for lexical sentiment processing
+  # perhaps do nltk vader for lexical sentiment processing as additional info for the model
   # might help the model train faster
-  def processText():
-    pass
+  def processText(text: str):
+    tokens = text.lower().split()
+
+    filtered_tokens = [token for token in tokens if token not in stopwords.words('english')]
+
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
