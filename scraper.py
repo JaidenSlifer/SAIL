8d2ae1bc2871749fed2f79a3c47a8aabcb0d6abe
tobiaskaczka <tobiaskaczka@gmail.com>
2024-03-21 18:45:16 -0400

    lol

--- scraper.py
+++ scraper.py
@@ -23,10 +23,9 @@ class ArticleScraper:
   # uses ticker instance variable and return a list of article links
   def getArticleLinks(self):
     # https://finviz.com/quote.ashx?t=TICKER&p=d
-    target_url = self.base_url.format(self.ticker)  # Assuming base_url expects to be formatted with the ticker
+    target_url = self.base_url.format(self.ticker)  # Assuming base_url to be formatted with the ticker
     self.driver.get(target_url)
 
-    # TODO: Need to tailor for site structure   
     link_elements = self.driver.find_elements(By.CSS_SELECTOR, 'tr.cursor-pointer.has-label a.tab-link-news')
         
     article_links = [element.get_attribute('href') for element in link_elements]
